3b599dc9811deedbf3c9af54eb0c816e04bf0ff6
DonatasNoreika <donoras@gmail.com>
2025-08-13 19:53:17 +0300

    Aprašytas userio profilio modelis, kuriame galima prisegti nuotrauką

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
 
@@ -11,3 +13,11 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to="profile_pics", blank=True, null=True)
+
+    def __str__(self):
+        return f"User {self.user.username} profile"
